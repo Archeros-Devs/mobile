7091a4a6f6844003a5425e6eb8db1e25b7c96001
Leonardo <leonardo.souza@aluno.faculdadeimpacta.com.br>
2020-09-18 01:29:55 -0300

    Cadastro de Pastas

--- app/src/main/java/br/com/arquerosdev/MainActivity.kt
+++ app/src/main/java/br/com/arquerosdev/MainActivity.kt
@@ -3,11 +3,15 @@ package br.com.arquerosdev
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import br.com.arquerosdev.viewmodel.UsuarioViewModel
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var usuarioViewModel: UsuarioViewModel
     private var sessaoUsuario: Boolean = false
@@ -17,14 +21,21 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        usuarioViewModel = ViewModelProvider(this).get(UsuarioViewModel::class.java)
-        usuarioViewModel.modelUsuarioLogado.observe(this, Observer { usuario ->
-            if(usuario != null){
-                sessaoUsuario = usuario.ativo!!
-            }
+        val spinner: Spinner = findViewById(R.id.spCategorias)
+        spinner.onItemSelectedListener = this
+            // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.categorias_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
 
-            Log.i(TAG,"UsuarioLoagado = ${sessaoUsuario}")
-        })
+
         if(sessaoUsuario){
             Log.i(TAG,"chama home")
         }else{
@@ -36,6 +47,15 @@ class MainActivity : BaseActivity() {
     override fun getUsuarioLogado() {
         super.getUsuarioLogado()
 
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
     }
 
 }
