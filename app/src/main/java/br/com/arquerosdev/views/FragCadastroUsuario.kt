747819065865a6343c6bcbe5f40a61b9d2976333
Fernando-Franco <fer.souza.franco@gmail.com>
2020-10-12 15:34:05 -0300

    Cadastro - funcoes check

--- app/src/main/java/br/com/arquerosdev/views/FragCadastroUsuario.kt
+++ app/src/main/java/br/com/arquerosdev/views/FragCadastroUsuario.kt
@@ -8,12 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import br.com.arquerosdev.R
 import br.com.arquerosdev.model.ModelEndereco
 import br.com.arquerosdev.model.ModelUsuario
+import br.com.arquerosdev.util.ChecarFuncoes
 import br.com.arquerosdev.viewmodel.EnderecoViewModel
 import br.com.arquerosdev.viewmodel.EscolaridadeViewModel
 import br.com.arquerosdev.viewmodel.ProfisaoViewModel
@@ -25,6 +27,7 @@ class FragCadastroUsuario : Fragment() {
 
     var idEscolaridade = 0
     var idProfissao = 0
+    lateinit var funcoes:ChecarFuncoes
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(R.layout.frag_cad_usuario, container, false)
@@ -32,9 +35,14 @@ class FragCadastroUsuario : Fragment() {
         configuracoesViewsProfissoes(view)
         configuracoesViewsEscolaridade(view)
 
+        funcoes = ChecarFuncoes(context!!)
+
         view.btSalvar.setOnClickListener {
-            val isValido = checarCampos(view)
-            if(isValido){salvarCadastro(view)}
+            if(view != null ){
+                val isValido = checarCampos(view)
+                //if(isValido){salvarCadastro(view)}
+            }
+
         }
         return view
     }
@@ -79,7 +87,7 @@ class FragCadastroUsuario : Fragment() {
                 val itemSelecionado = parent.getItemAtPosition(position).toString()
                 //Pegar id do Objeto na tabela
                 escolaridadeViewModel.modelEscolaridade.observe(activity!!, Observer{ idLinha ->
-                    idProfissao = idLinha.indexOfFirst { it.escolaridade == itemSelecionado}
+                    idEscolaridade = idLinha.indexOfFirst { it.escolaridade == itemSelecionado}
                 })
             }
             actvEscolaridade.onFocusChangeListener = View.OnFocusChangeListener{
@@ -88,9 +96,112 @@ class FragCadastroUsuario : Fragment() {
         })
     }
 
+    //TODO: Mover essa funcao para a classe ChecarFuncoes
     private fun checarCampos(view: View?): Boolean {
-        
-        return false
+        val strNomeCompleto = view!!.etNomeCompleto.text.toString()
+        if(strNomeCompleto.isBlank() || strNomeCompleto.length < 3){
+            view.etNomeCompleto.requestFocus()
+            view.etNomeCompleto.error = getText(R.string.msg_error_nome_completo)
+            return false
+        }
+
+        val strCPF = view.etCpf.text.toString()
+        if(strCPF.isBlank() || strCPF.length < 9 || funcoes.cpfNaoValido(strCPF)){
+            view.etCpf.requestFocus()
+            view.etCpf.error = getText(R.string.msg_error_cpf)
+            return false
+        }
+
+        val strEmail = view.etEmail.text.toString()
+        if(strEmail.isBlank() || strEmail.length < 8 || funcoes.emailNaoValido(strEmail)){
+            view.etEmail.requestFocus()
+            view.etEmail.error = getText(R.string.msg_error_email)
+            return false
+        }
+
+        //TODO: Colcoar componente para ver a senha
+        val strSenha = view.etSenha.text.toString()
+        if(strSenha.isBlank() || strSenha.length < 6){
+            view.etSenha.requestFocus()
+            view.etSenha.error = getText(R.string.msg_error_senha)
+            return false
+        }
+
+        //TODO: Colcoar componente para ver a senha
+        val strSenhaConf = view.etSenhaConf.text.toString()
+        if(strSenhaConf != strSenha){
+            view.etSenhaConf.requestFocus()
+            view.etSenhaConf.error = getText(R.string.msg_error_senha_conf)
+            return false
+        }
+
+        var checarGenero = view.genero_m.isChecked
+        if(!view.genero_m.isChecked && !checarGenero){
+            checarGenero = false
+        }
+        if(!view.genero_f.isChecked && !checarGenero){
+            checarGenero = false
+        }
+        if(!view.genero_outros.isChecked && !checarGenero){
+            checarGenero = false
+        }
+        if(!checarGenero){
+            view.genero_m.requestFocus()
+            Toast.makeText(activity,getText(R.string.msg_error_gerero),Toast.LENGTH_LONG).show()
+            return false
+        }
+
+        val strTelefone = view.etTelefone.text.toString()
+        if(strTelefone.isBlank() || strTelefone.length < 11){
+            view.etTelefone.requestFocus()
+            view.etTelefone.error = getText(R.string.msg_error_telefone)
+            return false
+        }
+
+        val strActvProfissao = view.actvProfissao.text.toString()
+        if(strActvProfissao.isBlank() || strActvProfissao.length < 2){
+            view.actvProfissao.requestFocus()
+            view.actvProfissao.error = getText(R.string.msg_error_profissao)
+            return false
+        }
+
+        val strActvEscolaridade = view.actvEscolaridade.text.toString()
+        if(strActvEscolaridade.isBlank() || strActvEscolaridade.length < 2){
+            view.actvEscolaridade.requestFocus()
+            view.actvEscolaridade.error = getText(R.string.msg_error_escolaridade)
+            return false
+        }
+
+        val strEtEndereco = view.etEndereco.text.toString()
+        if(strEtEndereco.isBlank() || strEtEndereco.length < 4){
+            view.etEndereco.requestFocus()
+            view.etEndereco.error = getText(R.string.msg_error_endereco)
+            return false
+        }
+
+        //TODO: Criar um validador de CEP
+        val strEtCep = view.etCep.text.toString()
+        if(strEtCep.isBlank() || strEtCep.length < 8){
+            view.etCep.requestFocus()
+            view.etCep.error = getText(R.string.msg_error_endereco)
+            return false
+        }
+
+        val strEtBairro = view.etBairro.text.toString()
+        if(strEtBairro.isBlank() || strEtBairro.length < 3){
+            view.etBairro.requestFocus()
+            view.etBairro.error = getText(R.string.msg_error_endereco)
+            return false
+        }
+
+        val strEtCidade = view.etCidade.text.toString()
+        if(strEtCidade.isBlank() || strEtCidade.length < 3){
+            view.etCidade.requestFocus()
+            view.etCidade.error = getText(R.string.msg_error_endereco)
+            return false
+        }
+
+        return true
     }
 
     fun salvarCadastro(view: View){
@@ -101,7 +212,7 @@ class FragCadastroUsuario : Fragment() {
 
         //view.etSenhaConf
         val usuario = ModelUsuario(
-            0,
+            0 ,//TODO: Nico gerar automatico no SERVIDOR
             idProfissao,
              view.etCpf.text.toString(),
              view.etNomeCompleto.text.toString(),
@@ -110,7 +221,7 @@ class FragCadastroUsuario : Fragment() {
              view.etEmail.text.toString(),
             "",
             idProfissao,
-            true,//TODO: VALIDAR DEPOIS -> ATIVAR DEPOIS NO LADO DO SERVIDOR
+            false,//TODO: Nico VALIDAR DEPOIS -> ATIVAR DEPOIS NO LADO DO SERVIDOR
             0,
             view.etTelefone.text.toString()
         )
@@ -119,11 +230,11 @@ class FragCadastroUsuario : Fragment() {
         //usuarioViewModel.insert(usuario)
 
         val endereco = ModelEndereco(
-            0,
+            0,//TODO: Nico gerar automatico no SERVIDOR
             0,
             view.etCidade.text.toString(),
             view.etEstado.text.toString(),
-            view.etCep.text.toString().toInt(),
+            0,//view.etCep.text.toString().toInt(),
             view.etEndereco.text.toString(),
             view.etNumero.text.toString(),
             view.etBairro.text.toString(),
@@ -133,9 +244,9 @@ class FragCadastroUsuario : Fragment() {
             .get(EnderecoViewModel::class.java)
         //enderecoViewModel.insert(endereco)
 
-        val frag = FragmentsLogin()
+    /*    val frag = FragmentsLogin()
             this.activity!!.supportFragmentManager.beginTransaction()
-            .replace(R.id.frag_main, frag, "Login")
+            .replace(R.id.frag_main, frag, "Login")*/
             //.commit()
     }
 }
