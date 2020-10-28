e5742dfed3fe465f2ed6c8749691b206cddf0a12
Fernando-Franco <fer.souza.franco@gmail.com>
2020-10-27 23:06:23 -0300

    Perfil Pasta - inicio

---
+++ app/src/main/java/br/com/arquerosdev/PerfilActivity.kt
@@ -0,0 +1,11 @@
+package br.com.arquerosdev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
