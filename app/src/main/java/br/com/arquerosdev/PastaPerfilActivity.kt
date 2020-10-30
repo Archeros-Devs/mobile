9cedde268d05e12168bb6a06450e89b2e4391f29
Leonardo <leonardo.souza@aluno.faculdadeimpacta.com.br>
2020-10-30 00:18:37 -0300

    Perfil pasta

--- app/src/main/java/br/com/arquerosdev/PastaPerfilActivity.kt
+++ app/src/main/java/br/com/arquerosdev/PastaPerfilActivity.kt
@@ -4,17 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import br.com.arquerosdev.model.ModelPasta
+import br.com.arquerosdev.viewmodel.UsuarioViewModel
 import kotlinx.android.synthetic.main.activity_pasta_perfil.*
 
 class PastaPerfilActivity : AppCompatActivity() {
+//    private var nameView:TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pasta_perfil)
 
         val pasta = intent.getParcelableExtra<ModelPasta>("pasta")
 
+        val usuarioViewModel = ViewModelProvider(this)
+            .get(UsuarioViewModel::class.java).modelUsuarioLogado
+        usuarioViewModel.observe(this, Observer { usuario ->
+            this.textNomeUsuario.text = usuario.nome
+        })
 
+        cardDiscusao.text = pasta?.discussao.toString()
+        cardDescricao.text = pasta?.descricao.toString()
+        cardlocalizacao.text = pasta?.localizacao.toString()
 
         button.setOnClickListener {
             val it = Intent(this@PastaPerfilActivity, PastaChatActivity::class.java)
