0226d77c301c3ed126617f70986eb484bf64146e
Nicholas Ferreira <nichoas_sc@live.com>
2020-11-13 00:32:26 -0300

    google maps

--- app/src/main/java/br/com/arquerosdev/MainActivity.kt
+++ app/src/main/java/br/com/arquerosdev/MainActivity.kt
@@ -4,22 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
-        btListarPastas.setOnClickListener { view ->
-            startActivity(Intent(this,PastaListaActivity::class.java))
-        }
-        btCriarPasta.setOnClickListener { view ->
-        }
+        //btListarPastas.setOnClickListener { view ->
+        //    startActivity(Intent(this,PastaListaActivity::class.java))
+        //}
 
-        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.pg_explorar -> {
                     // Respond to navigation item 1 click
@@ -27,7 +35,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.pg_criar -> {
                     startActivity(Intent(this, CriarPastaActivity::class.java))
-                    true
+                    false
                 }
                 R.id.pg_seguindo -> {
                     // Respond to navigation item 2 click
@@ -38,4 +46,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 }
