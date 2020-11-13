153bd29687cee462e0bf5a47792f5782195a2072
Nicholas Ferreira <nichoas_sc@live.com>
2020-11-12 23:56:19 -0300

    bottom navigation e splash screen

--- app/src/main/java/br/com/arquerosdev/MainActivity.kt
+++ app/src/main/java/br/com/arquerosdev/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +17,25 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this,PastaListaActivity::class.java))
         }
         btCriarPasta.setOnClickListener { view ->
-            startActivity(Intent(this, CriarPastaActivity::class.java))
+        }
+
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.pg_explorar -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.pg_criar -> {
+                    startActivity(Intent(this, CriarPastaActivity::class.java))
+                    true
+                }
+                R.id.pg_seguindo -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                else -> false
+            }
         }
     }
+
 }
