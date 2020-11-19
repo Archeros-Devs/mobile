12aa1795e88b26ff2c39abb8a89d6659275328ee
Nicholas Ferreira <nichoas_sc@live.com>
2020-11-18 21:37:38 -0300

    criar pasta com endereço

--- app/src/main/java/br/com/arquerosdev/MainActivityCrash.kt
+++ app/src/main/java/br/com/arquerosdev/MainActivityCrash.kt
@@ -3,11 +3,13 @@ package br.com.arquerosdev
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import android.os.Parcelable
 import android.util.Log
 import android.view.MenuItem
+import android.view.animation.DecelerateInterpolator
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.view.GravityCompat
@@ -21,8 +23,10 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnCameraMoveListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
@@ -31,14 +35,16 @@ import com.google.gson.JsonObject
 import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main_crash.*
 import kotlinx.android.synthetic.main.drawer_header.*
-import kotlin.concurrent.thread
 
 class MainActivityCrash : NavigationDrawer(), OnMapReadyCallback,
-    GoogleMap.OnInfoWindowClickListener {
+    GoogleMap.OnInfoWindowClickListener, GoogleMap.OnCameraIdleListener, OnCameraMoveListener {
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var lastLocation: Location
+    private lateinit var markerNovaPasta: Marker
     private var markerMap: HashMap<String, Int> = HashMap();
+    private val PeruibeLocation = LatLng(-24.3173, -46.9956)
+    private var modal = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,9 +55,12 @@ class MainActivityCrash : NavigationDrawer(), OnMapReadyCallback,
         btnDrawer.setOnClickListener { view -> drawer.openDrawer(GravityCompat.START) }
         drawer_user_name?.text = Prefs.getString("nome_usuario")
 
-        //btListarPastas.setOnClickListener { view ->
-        //    startActivity(Intent(this,PastaListaActivity::class.java))
-        //}
+        btnCloseModal.setOnClickListener { v -> onCloseModal()}
+        btnNovaPasta.setOnClickListener{ v ->
+            val it = Intent(this, CriarPastaActivity::class.java)
+            it.putExtra("localizacao", map.cameraPosition.target)
+            startActivity(it)
+        }
 
         bottom_navigation.setOnNavigationItemSelectedListener { item ->
             when(item.itemId) {
@@ -60,7 +69,7 @@ class MainActivityCrash : NavigationDrawer(), OnMapReadyCallback,
                     true
                 }
                 R.id.pg_criar -> {
-                    startActivity(Intent(this, CriarPastaActivity::class.java))
+                    onCriarPasta()
                     false
                 }
                 R.id.pg_seguindo -> {
@@ -72,17 +81,31 @@ class MainActivityCrash : NavigationDrawer(), OnMapReadyCallback,
         }
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-        map.getUiSettings().setZoomControlsEnabled(true)
-        map.setOnInfoWindowClickListener(this)
-        val currentLatLng = LatLng(-24.3173, -46.9956)
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 14f))
-        setMapMarkers()
-        setUpMap()
+    override fun onResume() {
+        super.onResume()
+        callPastas()
     }
 
-    private fun setMapMarkers() {
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.uiSettings.isZoomControlsEnabled = true
+        map.setOnInfoWindowClickListener(this)
+        map.setOnCameraIdleListener(this);
+        map.setOnCameraMoveListener(this);
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(PeruibeLocation, 14f))
+        markerNovaPasta = map.addMarker(
+            MarkerOptions()
+                .position(map.cameraPosition.target)
+                .draggable(true)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .title("Nova Pasta")
+                .visible(false)
+        )
+        setPastasMarkers()
+        findMyLocation()
+    }
+
+    private fun setPastasMarkers() {
         val pastaViewModel: PastaViewModel = ViewModelProvider(this).get(PastaViewModel::class.java)
         pastaViewModel.modelPasta.observe(this, Observer { listaPasta ->
             listaPasta.forEach {
@@ -90,7 +113,7 @@ class MainActivityCrash : NavigationDrawer(), OnMapReadyCallback,
                     placeMarkerOnMap(
                         it.id_pasta,
                         it.nome!!,
-                        it.descricao!!,
+                        it.discussao!!,
                         LatLng(it.latitude, it.longitude)
                     )
                 }
@@ -98,7 +121,7 @@ class MainActivityCrash : NavigationDrawer(), OnMapReadyCallback,
         })
     }
 
-    private fun setUpMap() {
+    private fun findMyLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED
             && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -130,6 +153,23 @@ class MainActivityCrash : NavigationDrawer(), OnMapReadyCallback,
         markerMap.put(marker.id, id_pasta)
     }
 
+    override fun onInfoWindowClick(marker: Marker?) {
+        if (marker == null) return
+        Log.e("teste", marker.title.toString())
+
+        val id_pasta: Int = markerMap[marker.id] ?: return
+        val pastaViewModel: PastaViewModel = ViewModelProvider(this).get(PastaViewModel::class.java)
+        Thread {
+            val pasta = pastaViewModel.getPasta(id_pasta)
+            runOnUiThread {
+                val it = Intent(this@MainActivityCrash, PastaPerfilActivity::class.java)
+                it.putExtra("pasta", pasta as Parcelable)
+                startActivity(it)
+            }
+        }.start()
+
+    }
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId){
             R.id.nav_favoritos -> null
@@ -138,9 +178,27 @@ class MainActivityCrash : NavigationDrawer(), OnMapReadyCallback,
         return true
     }
 
-    override fun onResume() {
-        super.onResume()
-        callPastas()
+    private fun onCriarPasta() {
+        //startActivity(Intent(this, CriarPastaActivity::class.java))
+        modal = true
+        cardCriarPasta.animate()
+            .translationY(0F)
+            .alpha(1F)
+            .setDuration(500)
+            .setInterpolator(DecelerateInterpolator())
+
+        markerNovaPasta.isVisible = true
+        markerNovaPasta.position = map.cameraPosition.target
+    }
+
+    private fun onCloseModal() {
+        modal = false
+        markerNovaPasta.isVisible = false
+        cardCriarPasta.animate()
+            .translationY(200F)
+            .alpha(0F)
+            .setDuration(500)
+            .setInterpolator(DecelerateInterpolator())
     }
 
     private fun callPastas(){
@@ -169,20 +227,26 @@ class MainActivityCrash : NavigationDrawer(), OnMapReadyCallback,
         }
     }
 
-    override fun onInfoWindowClick(marker: Marker?) {
-        if (marker == null) return
-        Log.e("teste", marker.title.toString())
+    override fun onBackPressed() {
+        if(modal) onCloseModal()
+        else super.onBackPressed()
+    }
 
-        val id_pasta: Int = markerMap[marker.id] ?: return
-        val pastaViewModel: PastaViewModel = ViewModelProvider(this).get(PastaViewModel::class.java)
-        Thread {
-            val pasta = pastaViewModel.getPasta(id_pasta)
-            runOnUiThread {
-                val it = Intent(this@MainActivityCrash, PastaPerfilActivity::class.java)
-                it.putExtra("pasta", pasta as Parcelable)
-                startActivity(it)
-            }
-        }.start()
+    private fun getAddress(latLng: LatLng): String {
+        val geocoder = Geocoder(this)
+        val list = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+        val addressText = list[0].getAddressLine(0)
+        return addressText
+    }
 
+    override fun onCameraMove() {
+        if (modal) markerNovaPasta.position = map.cameraPosition.target
+    }
+
+    override fun onCameraIdle() {
+        if (modal) {
+            val address = getAddress(map.cameraPosition.target)
+            text_endereco.text = address
+        }
     }
 }
